aa320976f71d2e9eb85a428caa410fa81be16ba3
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-16 00:42:23 +0300

    Create pall.c

---
+++ pall.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+
+/**
+ * pall - Prints all values on the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number in the Monty bytecode file.
+ */
+void pall(stack_t **stack, unsigned int __attribute__((unused)) line_number)
+{
+    stack_t *current = *stack;
+
+    if (current == NULL)
+		return;
+
+    /* Print all values on the stack */
+    while (current)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
