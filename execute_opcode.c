ade2b0befbd3287eba42fbd7784b95de7bf520cb
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2023-11-16 00:42:18 +0300

    Create execute_opcode.c

---
+++ execute_opcode.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * execute_opcode - Execute Monty opcode based on the parsed line.
+ * @opcode: The opcode to execute.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number in the Monty bytecode file.
+ */
+void execute_opcode(const char *opcode, stack_t **stack, unsigned int line_number)
+{
+    instruction_t op_funcs[] = {
+        {"pall", pall},
+        {NULL, NULL}
+    };
+
+    for (int i = 0; op_funcs[i].opcode; i++)
+    {
+        if (strcmp(opcode, op_funcs[i].opcode) == 0)
+        {
+            if (op_funcs[i].f == NULL)
+            {
+                fprintf(stderr, "L%u: Function %s not implemented\n", line_number, opcode);
+                exit(EXIT_FAILURE);
+            }
+            op_funcs[i].f(stack, line_number);
+            return;
+        }
+    }
+
+    fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+    exit(EXIT_FAILURE);
+}
